1037caf63898c860c5b23e6e023c7d0a38ac6b18
ademblon <62746143+ademblon@users.noreply.github.com>
2023-07-12 18:08:08 +0200

    first try at dynamic team opinion

---
+++ modding_tool/france_constitution_ig_law_modding_tool.py
@@ -0,0 +1,5 @@
+laws = ["law_executive","law_parliamentary","law_council_elective","law_representative_democracy", "law_consensus_democracy", "law_direct_democracy", "law_unitary", "law_devolved", "law_federalized", "law_free_market", "law_interventionism", "law_socialization"]
+
+
+for law in laws :
+    print("france_constitution_ig_stance_value_{law}_value = {{\n    value = 1\n    if = {{ limit = {{ law_stance = {{law = law_type:{law} value = strongly_approve}}}}\n        add = 5\n    }}\n    else_if = {{ limit = {{ law_stance = {{law = law_type:{law}  value = approve}}}}\n        add = 3\n    }}\n    else_if = {{ limit = {{ law_stance = {{law = law_type:{law}  value = disapprove}}}}\n        subtract = 1\n    }}\n    else_if = {{ limit = {{ law_stance = {{law = law_type:{law}  value = strongly_disapprove}}}}\n        subtract = 3\n    }}\n}}".format(law = law))
